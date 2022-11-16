0759ee65b3d4358536d0845794474de75f017611
shoebabedin <shoeb.abedin@intelli.global>
2022-11-16 10:35:54 +0600

    accrodion toggle add

--- components/Common/Accordion/Accordion.js
+++ components/Common/Accordion/Accordion.js
@@ -2,7 +2,7 @@ import { MinusCircle, PlusCircle } from "phosphor-react";
 import React, { useEffect, useState } from "react";
 
 const Accordion = () => {
-  const [accordionOpen, setAccordionOpen] = useState(false);
+  const [accordionOpen, setAccordionOpen] = useState(0);
   const [accordion, setAccordion] = useState({});
 
   useEffect(() => {
@@ -10,6 +10,7 @@ const Accordion = () => {
       .then((res) => res.json())
       .then((differnet) => setAccordion(differnet));
   }, []);
+
 
   return (
     <>
@@ -31,39 +32,47 @@ const Accordion = () => {
               {/* <!-- accordion --> */}
               <div className="accordion">
                 <div className="grid grid-cols-12 gap-4">
-                  {accordion.length > 0 && accordion.map((item, index) => (
-                    <div
-                      id={item.id}
-                      key={index}
-                      className="col-span-12 lg:col-span-6"
-                    >
-                      <li
-                        onClick={() => setAccordionOpen(item.id)}
-                        className="question flex items-center justify-between px-3"
-                        id="question"
+                  {accordion.length > 0 &&
+                    accordion.map((item, index) => (
+                      <div
+                        id={item.id}
+                        key={index}
+                        className="col-span-12 lg:col-span-6"
                       >
-                        <p className="font-medium text-base md:text-xl leading-[30px] text-[#393e50]">
-                          {item.ques}
-                        </p>
+                        <li
+                          onClick={() => setAccordionOpen(accordionOpen === item.id  ? "" : item.id)}
+                          className="question flex items-center justify-between px-3"
+                          id="question"
+                        >
+                          <p className="font-medium text-base md:text-xl leading-[30px] text-[#393e50]">
+                            {item.ques}
+                          </p>
 
-                        {
-                          accordionOpen === item.id ? 
-                          <MinusCircle size={28} color="#30A7D7" weight="light" />
-                          :
-                          <PlusCircle size={28} color="#393e50" weight="light" />
-                        }
-                      </li>
-                      <li
-                        className={`font-normal text-base leading-6 text-[rgba(57,62,80,0.4)] list-none ${
-                          accordionOpen === item.id
-                            ? "translate-y-0 ease-in-out opacity-100 transition-all duration-300 pt-4 pr-2 pb-5 md:pb-10"
-                            : "opacity-0 -translate-y-4 transition-all duration-300 h-0"
-                        }`}
-                      >
-                        {item.ans}
-                      </li>
-                    </div>
-                  ))}
+                          {accordionOpen === item.id ? (
+                            <MinusCircle
+                              size={28}
+                              color="#30A7D7"
+                              weight="light"
+                            />
+                          ) : (
+                            <PlusCircle
+                              size={28}
+                              color="#393e50"
+                              weight="light"
+                            />
+                          )}
+                        </li>
+                        <li
+                          className={`font-normal text-base leading-6 text-[rgba(57,62,80,0.4)] list-none ${
+                            accordionOpen === item.id
+                              ? "translate-y-0 ease-in-out opacity-100 transition-all duration-300 pt-4 pr-2 pb-5 md:pb-10"
+                              : "opacity-0 -translate-y-4 transition-all duration-300 h-0"
+                          }`}
+                        >
+                          {item.ans}
+                        </li>
+                      </div>
+                    ))}
                 </div>
               </div>
               {/* <!-- accordion --> */}
